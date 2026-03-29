b619d89642fcafcd9ddbba0e60238756c1af3b73
DaltonStone88 <stonemdalton@gmail.com>
2026-03-28 22:29:24 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core'],
+  experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core'],
+  },
 }
 module.exports = nextConfig
